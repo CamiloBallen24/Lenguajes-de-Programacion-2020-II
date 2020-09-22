726497cf3194dd0230971eacbf4a6f647ed73ed6
jdacostabe <jdacostabe@unal.edu.co>
2020-09-22 17:48:05 -0500

    Integracion completa

--- src/Lexer/Processor.java
+++ src/Lexer/Processor.java
@@ -1,6 +1,5 @@
 package Lexer;
 
-
 import java.util.ArrayList; 
  import java.util.Random;
 
@@ -13,6 +12,7 @@ public class Processor {
     
     public ArrayList<Token> tokens;
     
+    public Reader reader;
     public State current_state;
     public BCCGraph graph;
     
@@ -29,24 +29,23 @@ public class Processor {
     public  String cadena = "BA\n A21++A \t 33.01 #2.1";
     
     
-    
-    public Processor(){
+    public Processor(String fileName){
+        this.reader = new Reader(fileName);
+        
         this.buffer = "";
         this.string_read = "";
         this.initial_column = 0;
         this.initial_line = 0;
         
-        this.tokens = new ArrayList<Token>();
+        this.tokens = new ArrayList<>();
         
         this.graph = new BCCGraph();
-        this.current_state = graph.initial_state;
+        this.current_state = BCCGraph.initial_state;
     }
-    
-    
     
     public String nextToken(){
         //Reiniciar Cosas
-        this.current_state = this.graph.initial_state;
+        this.current_state = BCCGraph.initial_state;
         this.string_read = "";
         
         this.last_state_valide = null;
@@ -54,26 +53,26 @@ public class Processor {
         
         
         //Va leyendo el codigo fuente hasta finalizar el token
-        Character current_character = this.nextChar();
+        Character current_character;
+        do{
+            current_character = this.nextChar();
+            this.initial_column = reader.column;
+            this.initial_line = reader.line;
+        }while(BCCProperties.characters_to_ignore.contains(current_character));
         
         while((current_character != null)){
-            
             
             this.current_state = nextState(current_character); 
             
             //Ignorar comentarios
             if(BCCProperties.characters_line_comment.contains(current_character)){
-                //Hacer el santo de linea
-                this.saltarLineaFake();
-                
+                this.reader.nextLine();
             }
             
             //Ignorar espacio, tabulador, salto de linea y comentario pero contar estado
             if(!BCCProperties.characters_to_ignore.contains(current_character)){
                 this.string_read = this.string_read + current_character;
             }
-            
-            
             
             if(this.current_state == null){break;}
             
@@ -84,52 +83,38 @@ public class Processor {
             current_character = this.nextChar();
         }
         
-        
         //Define si existio algun estado valido (casi siempre el ultimo)
         if(this.last_state_valide != null){
             //Generar Token Para ese estado
-            this.generateToken(this.last_state_valide, this.last_string_valide, 0, 0);
+            this.generateToken(this.last_state_valide, this.last_string_valide, this.initial_line, this.initial_column);
             
             //Manda lo sobrante al buffer
-            this.buffer = this.string_read.substring(this.last_string_valide.length(), this.string_read.length());
+            this.buffer = this.string_read.substring(this.last_string_valide.length());
         }
-        
-        
         
         //Reportar error
         else if (this.current_state == null){
-            this.generateError(this.string_read, 0, 0);
+            this.generateError(this.string_read, this.initial_line, this.initial_column);
         }
         
         else if(this.current_state.type_state.equals("initial")){
         
         }
-        
-        
         return null;
     }
-    
     
     public Character nextChar(){
         if(this.buffer.length() > 0){
             Character character =  this.buffer.charAt(0);
-            this.buffer = this.buffer.substring(1, this.buffer.length());
+            this.buffer = this.buffer.substring(1);
             return character;
-        }
-        else{
-            return lectorNextCharFake();
+        }else{
+            return reader.nextChar();
         }
     }
-   
-
-    
-
-    
-    
-    
     
     public State nextState(Character character){
-        ArrayList<Connection> connections_current_state = this.graph.connectionsByState(this.current_state);
+        ArrayList<Connection> connections_current_state = BCCGraph.connectionsByState(this.current_state);
         
         for (Connection connection : connections_current_state) {
             if(connection.chars_options.contains(character)){
@@ -139,35 +124,12 @@ public class Processor {
         return null;
     }
     
-    
     public Token generateToken(State token_state, String token_string, int line, int column){
-        System.out.println(token_state.type_state + "  " + token_string);
+        System.out.println(token_state.type_state + "  " + token_string+"\nLine: "+line+"   Column: "+column);
         return null;
     }
     
-    public void generateError(String error_string, int line, int column){    
-        System.out.println("ERROR" + "  " + error_string);
+    public void generateError(String error_string, int line, int column){
+        System.out.println("ERROR " + error_string + "\nIn line: "+line+"\nIn column: "+column);
     }
-    
-    
-    //COSAS FAKES
-    
-    //Simula el salto de linea
-    public void saltarLineaFake(){
-        this.cursor = 0;
-        this.cadena = "AAAABBABABAED 2.12";
-    }
-    
-    
-    public Character lectorNextCharFake(){
-        try{
-            Character character = this.cadena.charAt(cursor);
-            this.cursor++;
-            return character;
-        } 
-        catch(Exception e){
-            return  null;
-        }
-    }
-    
 }
