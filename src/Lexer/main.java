53e9c6656357c1c4fd764701ff78ada61c5a5689
jdacostabe <48451314+jdacostabe@users.noreply.github.com>
2020-09-25 20:49:57 -0500

    Arreglo lectura archivo

--- src/Lexer/main.java
+++ src/Lexer/main.java
@@ -1,11 +1,26 @@
 package Lexer;
-import java.util.Random;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import Lexer.*;
 
 public class main {
     public static void main(String[] args) {
-        Processor procesador = new Processor("D:\\Desktop\\Prueba.txt");
+        
+        Scanner entradaEscaner = new Scanner (System.in);
+      
+        try {
+            FileWriter escritorArchivo = new FileWriter("src/codigo.txt");
+            while(entradaEscaner.hasNext()) {
+                escritorArchivo.write(entradaEscaner.nextLine()+"\n");
+            }
+            escritorArchivo.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        
+        Processor procesador = new Processor("src/codigo.txt");
         procesador.nextToken();
         procesador.nextToken();
         procesador.nextToken();
