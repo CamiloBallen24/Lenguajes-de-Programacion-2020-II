beece8821c5b74923a91ea4fcfa040d992271747
Andrés Felipe Castillo Sopó <46506916+Saicod@users.noreply.github.com>
2020-10-13 08:22:45 -0500

    Implementacion Predicciones

--- src/Syntax/Models/GrammarRule.java
+++ src/Syntax/Models/GrammarRule.java
@@ -1,13 +1,19 @@
 package Syntax.Models;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class GrammarRule {
     public GrammarNoTerminal left_part;
     public ArrayList<GrammarSymbol> right_part;
+    public HashSet<GrammarTerminal> predictions;
 
     public GrammarRule(GrammarNoTerminal left_part, ArrayList<GrammarSymbol> right_part) {
         this.left_part = left_part;
         this.right_part = right_part;
+    }
+
+    public void setPredictions(HashSet<GrammarTerminal> prediction){
+        this.predictions = prediction;
     }
 }
